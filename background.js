eb5f3af8ab31539b0ae0bf1043313d45c1af9665
Ned Watson <watsonle@iastate.edu>
2023-03-03 00:55:44 -0500

    redirect skyrim.fandom.com pages to Skyrim namespace in UESP
    This commit aims to address another major category of pages which the
    original extension leaves unaltered: typically, pages on the Skyrim
    wiki will belong to the "Skyrim:" namespace on the UESP wiki.
    
    Doing this cleanly requires a significant refactor of the code as
    previously written.  As a result, the namespace and page name are now
    determined seperately, rather than all being found at once with
    a single regex.  This has not caused any new bugs to emerge, at least
    not in my testing.
    
    Subsequent to this commit, two more changes are expected:
    * Tests will be created to verify that my revisions have not created
      any major bugs
    * One additional edge case will be accounted for --- the case when
      the user navigates directly to "elderscrolls.fandom.com" or
      "skyrim.fandom.com", which should redirect to the UESP home page
      and the Skyrim mainspace portal, respectively.

--- background.js
+++ background.js
@@ -5,10 +5,19 @@
   let storage = (typeof chrome.storage === "undefined") ? browser.storage : chrome.storage; // Check to see if using Chrome or Firefox
 
   const WIKIA_REGEX = /^(elderscrolls|skyrim)\.(wikia|fandom)\.com$/i; // Used to match the domain of the old wikia/fandom to make sure we are redirecting the correct domain.
-  // Used to match what game is in the title. This will be used to redirect to the appropriate page. For example, on Wikia it might be Diseases (Skyrim) while on UESP it has to be formated to Skyrim:Diseases
-  const GAMES_REGEX = /\(Blades\)|\(Legends\)|\(Online\)|\(Skyrim\)|\(Dawnguard\)|\(Hearthfire\)|\(Dragonborn\)|\(Oblivion\)|\(Knights of the Nine\)|\(Morrowind\)|\(Tribunal\)|\(Bloodmoon\)|\(Daggerfall\)|\(Arena\)/
+  const SKYRIM_REGEX = /^skyrim\./i // used to determine whether on the Skyrim wiki
+
+  /*
+   NAMESPACE_REGEX determines which game is in the page title. This will 
+   be used to  redirect to the appropriate UESP page. For example, on 
+   Wikia it might be  Diseases (Skyrim) while on UESP it has to be 
+   formated to  Skyrim:Diseases.  This version of the expression has 
+   been modified to take advantage of capture groups. 
+   */
+  const NAMESPACE_REGEX = /(.+?)\((Blades|Legends|Online|Skyrim|Dawnguard|Hearthfire|Dragonborn|Oblivion|Knights of the Nine|Morrowind|Tribunal|Bloodmoon|Daggerfall|Arena)\)(.*?)$/
   // Used to account for edge case where user is navigating to the Wikia/Fandom homepage for a given game
   const HOMEPAGE_REGEX = /^Portal:(.+)$/;
+
   // Listen to before anytime the browser attempts to navigate to the old Wikia/Fandom sites.
   chrome.webNavigation.onBeforeNavigate.addListener(
     function(info) {
@@ -17,30 +26,48 @@
         console.log("Elder Scrolls Wikia intercepted, ignoring because plugin is disabled.");
         return;
       }
-
       // Create a native URL object to more easily determine the path of the url and the domain.
       const url = new URL(info.url);
 
       const isWikia = WIKIA_REGEX.test(url.host); // Check whether the request is occuring on either the fandom/wikia domain.
       // If domain isn't a subdomain of wikia.com, ignore, also if it's not in the redirect filter
-      if (!isWikia) return;
+      if (!isWikia) return;      
 
-      var urlChange = url.pathname.replace('/wiki/', '').replace(/_/g, '+');  // Change URL from underscores to plus symbols so that it can search the wiki. Also removes /wiki/ from the URL.
-      var game = url.pathname.match(GAMES_REGEX); // Find if Skyrim
-      if (game) { // Check if game exists (is not null)
-        game = String(game); // Cast to a string so we can manipulate it
-        urlChange = game.replace(/[()]/g,'') + ':' + urlChange.replace(game, ''); // Remove the parathensis, add : and then the game. This will format as e.g. Oblivion:Spells
-      } else {
-        const homepageMatch = urlChange.match(HOMEPAGE_REGEX);
+      let wikiaPageName = url.pathname.replace('/wiki/', '').replace(/_/g, '+');  // Change URL from underscores to plus symbols so that it can search the wiki. Also removes /wiki/ from the URL.
+
+
+      // # Determine title of the UESP page
+      let namespace = '', uespPageName = '';
+
+      // ## Determine `namespace`
+      let titleInfo = wikiaPageName.match(NAMESPACE_REGEX);
+
+      if (titleInfo) { 
+        namespace = titleInfo[2]+':';
+      } else if (url.host.match(SKYRIM_REGEX)) { // skyrim wiki should be in 'Skyrim:' namespace
+        namespace = 'Skyrim:'
+      } else { // if ES wiki and titled "Portal:[X]", UESP page is probably titled "[X]:[X]"
+        const homepageMatch = wikiaPageName.match(HOMEPAGE_REGEX);
         if (homepageMatch) {
-          urlChange = `${homepageMatch[1]}:${homepageMatch[1]}`;
+          namespace = homepageMatch[1]+':';
+          uespPageName = homepageMatch[1];
         }
       }
       
+      // ## Determine `uespPageName` if not already found
+      if (!uespPageName) {
+        if (titleInfo) {
+          uespPageName = 
+            titleInfo[1].replace(/(^\+*)|(\+*$)/g, '') +
+            titleInfo[3].replace(/(\+*$)/g, '');
+        } else {
+          uespPageName = wikiaPageName;
+        }
+      }
 
       // Generate new url
       const host = 'en.uesp.net/?search=';
-      const redirectUrl = `https://${host}${urlChange}`; // Create the redirect URL
+      const redirectUrl = `https://${host}${namespace}${uespPageName}`; // Create the redirect URL
       console.log(`Elder Scrolls Wikia intercepted:  ${info.url}\nRedirecting to ${redirectUrl}`); 
       // Redirect the old wikia request to the UESP wiki
       chrome.tabs.update(info.tabId,{url:redirectUrl});
