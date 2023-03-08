e9854a428ddc8790c4794f8f8c2000e55eb02508
thelearned1 <learned.watson@protonmail.com>
2023-03-08 12:26:17 -0500

    add tests for homepage redirect functionality

--- __tests__/homepage.test.js
+++ __tests__/homepage.test.js
@@ -1,10 +1,25 @@
+const fs = require('fs');
+const getUespPage = require('../getUespPage');
+
+const elderscrollsWiki = 'https://elderscrolls.fandom.com/wiki/'
 const portalPages = JSON.parse(fs.readFileSync('./__tests__/testData/homepage-test-entries.json'));
-const homePages = JSON.parse(fs.readFileSync('./__tests__/testData/homepage-test-entries.json'));
+const mainPages = JSON.parse(fs.readFileSync('./__tests__/testData/main-page-test-entries.json'));
+describe('elderscrolls.fandom.com portal pages', () => {
+	test.each(portalPages)(
+		' $fandomTitle is redirected to $uespTitle', 
+		({id, fandomTitle, uespTitle}) => {
+			const urlChange = getUespPage(new URL(`${elderscrollsWiki}${fandomTitle.replace(/ /g, '_')}`));
+			const output = urlChange.replace(/\+/g, ' ').trim();
+			expect(output).toBe(uespTitle);
+		}
+	)
+})
+
 describe('URLs that point to the main page of either wiki', () => {
-	test.each(homePages)(
+	test.each(mainPages)(
 		' $fandomUrl is redirected to UESP page $uespTitle',
 		({ fandomUrl, uespTitle }) => {
-			const url = getUespPage(new URL(fandomUrl));
+			const urlChange = getUespPage(new URL(fandomUrl));
 			const output = urlChange.replace(/\+/g, ' ').trim();
 			expect(output).toBe(uespTitle);
 		}
